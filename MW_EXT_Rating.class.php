271f6a8e71a473bfbfa885d0291f8468dee68312
Kitsune Solar <kitsune.solar@gmail.com>
2018-06-12 03:34:43 +0300

    Update resource

--- MW_EXT_Rating.class.php
+++ MW_EXT_Rating.class.php
@@ -146,6 +146,11 @@ class MW_EXT_Rating {
 				$value            = self::clearData( $pair[1] );
 				$results[ $name ] = $value;
 			}
+
+			if ( count( $pair ) === 1 ) {
+				$name             = self::clearData( $pair[0] );
+				$results[ $name ] = true;
+			}
 		}
 
 		return $results;
