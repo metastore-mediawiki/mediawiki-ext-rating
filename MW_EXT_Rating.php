43fb79a74183ea88507b55cbb49e4dce5fbb715c
Kitsune Solar <kitsune.solar@gmail.com>
2018-07-20 15:44:36 +0300

    2018-07-20 12:44:35

--- MW_EXT_Rating.php
+++ MW_EXT_Rating.php
@@ -7,15 +7,10 @@ if ( ! defined( 'MEDIAWIKI' ) ) {
 
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'MW_EXT_Rating' );
-	// Keep i18n globals so mergeMessageFileList.php doesn't break
+	// Keep i18n globals so mergeMessageFileList.php doesn't break.
 	$wgExtensionMessagesFiles['MW_EXT_Rating'] = __DIR__ . '/i18n';
 
-	/* wfWarn(
-	  'Deprecated PHP entry point used for MW_EXT_Rating extension. Please use wfLoadExtension instead, ' .
-	  'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
-
-	return;
+	return true;
 } else {
-	die( 'This version of the MW_EXT_Rating extension requires MediaWiki 1.25+' );
+	die( 'This version of the MW_EXT_Rating extension requires MediaWiki 1.30+' );
 }
